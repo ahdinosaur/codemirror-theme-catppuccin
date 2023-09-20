4a3afbe32ea3017867d90de66bef28d5563a9ae2
Michael Williams <michael.williams@enspiral.com>
2023-09-20 16:33:58 +1200

    complete overhaul of language syntax highlighting

--- src/index.ts
+++ src/index.ts
@@ -130,65 +130,108 @@ export function catppuccinTheme(variant: Variant) {
   )
 }
 
+// - https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/syntax.lua
+// - https://github.com/catppuccin/vscode/blob/main/src/theme/tokens/index.ts
+
 /// The highlighting style for code in the Catppuccin theme.
 export function catppuccinHighlightStyle(variant: Variant) {
   const palette: Palette = variants[variant]
 
   return HighlightStyle.define([
-    // keyword
-    { tag: [t.keyword, t.bool], color: palette.mauve.hex },
-    // string
-    {
-      tag: [t.processingInstruction, t.string, t.inserted],
-      color: palette.green.hex,
-    },
-    // escape sequences
-    {
-      tag: [t.escape, t.regexp, t.special(t.string)],
-      color: palette.pink.hex,
-    },
-    // comments
-    { tag: [t.comment], color: palette.overlay0.hex },
-    // constants, numbers
-    {
-      tag: [t.number, t.atom, t.color, t.constant(t.name), t.standard(t.name)],
-      color: palette.peach.hex,
-    },
-    // operators
-    { tag: [t.operator, t.operatorKeyword], color: palette.sky.hex },
-    // braces, delimiters
-    { tag: [t.deleted, t.character, t.separator], color: palette.overlay2.hex },
-    // methods, functions
-    { tag: [t.function(t.variableName), t.labelName], color: palette.blue.hex },
-    // parameters
-    {
-      tag: [t.propertyName, t.macroName],
-      color: palette.maroon.hex,
-    },
-    // local variables
-    {
-      tag: [t.special(t.variableName), t.definition(t.name), t.name],
-      color: palette.teal.hex,
-    },
-    // built-ins
-
-    // classes, metadata
-    {
-      tag: [t.meta, t.typeName, t.className, t.annotation, t.modifier, t.self, t.namespace],
-      color: palette.yellow.hex,
-    },
-    // link
-    {
-      tag: [t.url, t.link],
-      color: palette.sky.hex,
-      textDecoration: 'underline',
-    },
-    // other
-    { tag: t.strong, fontWeight: 'bold' },
+    { tag: t.comment, color: palette.overlay0.hex },
+    { tag: t.lineComment, color: palette.overlay0.hex },
+    { tag: t.blockComment, color: palette.overlay0.hex },
+    { tag: t.docComment, color: palette.overlay0.hex },
+    { tag: t.name, color: palette.flamingo.hex },
+    { tag: t.variableName, color: palette.flamingo.hex },
+    { tag: t.typeName, color: palette.yellow.hex },
+    { tag: t.tagName, color: palette.lavender.hex },
+    { tag: t.propertyName, color: palette.teal.hex },
+    { tag: t.attributeName, color: palette.teal.hex },
+    { tag: t.className, color: palette.yellow.hex },
+    { tag: t.labelName, color: palette.sapphire.hex },
+    { tag: t.namespace, color: palette.yellow.hex },
+    { tag: t.macroName, color: palette.mauve.hex },
+    { tag: t.literal, color: palette.peach.hex },
+    { tag: t.string, color: palette.green.hex },
+    { tag: t.docString, color: palette.green.hex },
+    { tag: t.character, color: palette.teal.hex },
+    { tag: t.attributeValue, color: palette.teal.hex },
+    { tag: t.number, color: palette.peach.hex },
+    { tag: t.integer, color: palette.peach.hex },
+    { tag: t.float, color: palette.peach.hex },
+    { tag: t.bool, color: palette.peach.hex },
+    { tag: t.regexp, color: palette.pink.hex },
+    { tag: t.escape, color: palette.pink.hex },
+    { tag: t.color, color: palette.peach.hex },
+    { tag: t.url, color: palette.blue.hex, textDecoration: 'underline' },
+    { tag: t.keyword, color: palette.mauve.hex },
+    { tag: t.self, color: palette.red.hex },
+    { tag: t.null, color: palette.mauve.hex },
+    { tag: t.atom, color: palette.mauve.hex },
+    { tag: t.unit, color: palette.mauve.hex },
+    { tag: t.modifier, color: palette.mauve.hex },
+    { tag: t.operatorKeyword, color: palette.teal.hex },
+    { tag: t.controlKeyword, color: palette.yellow.hex, fontWeight: 'bold' },
+    { tag: t.definitionKeyword, color: palette.yellow.hex },
+    { tag: t.moduleKeyword, color: palette.mauve.hex },
+    { tag: t.operator, color: palette.sky.hex },
+    { tag: t.derefOperator, color: palette.sky.hex },
+    { tag: t.arithmeticOperator, color: palette.sky.hex },
+    { tag: t.logicOperator, color: palette.sky.hex },
+    { tag: t.bitwiseOperator, color: palette.sky.hex },
+    { tag: t.compareOperator, color: palette.sky.hex },
+    { tag: t.updateOperator, color: palette.sky.hex },
+    { tag: t.definitionOperator, color: palette.sky.hex },
+    { tag: t.typeOperator, color: palette.sky.hex },
+    { tag: t.controlOperator, color: palette.sky.hex },
+    { tag: t.punctuation, color: palette.overlay2.hex },
+    { tag: t.separator, color: palette.overlay2.hex },
+    { tag: t.bracket, color: palette.overlay2.hex },
+    { tag: t.angleBracket, color: palette.overlay2.hex },
+    { tag: t.squareBracket, color: palette.overlay2.hex },
+    { tag: t.paren, color: palette.overlay2.hex },
+    { tag: t.brace, color: palette.overlay2.hex },
+    { tag: t.content, color: palette.text.hex },
+    { tag: t.heading, color: palette.mauve.hex, fontWeight: 'bold' },
+    { tag: t.heading1, color: palette.red.hex, fontWeight: 'bold' },
+    { tag: t.heading2, color: palette.peach.hex, fontWeight: 'bold' },
+    { tag: t.heading3, color: palette.yellow.hex, fontWeight: 'bold' },
+    { tag: t.heading4, color: palette.green.hex, fontWeight: 'bold' },
+    { tag: t.heading5, color: palette.blue.hex, fontWeight: 'bold' },
+    { tag: t.heading6, color: palette.mauve.hex, fontWeight: 'bold' },
+    { tag: t.contentSeparator, color: palette.teal.hex },
+    { tag: t.list, color: palette.teal.hex },
+    { tag: t.quote, color: palette.pink.hex, fontStyle: 'italic' },
     { tag: t.emphasis, fontStyle: 'italic' },
+    { tag: t.strong, fontWeight: 'bold' },
+    { tag: t.link, color: palette.lavender.hex, textDecoration: 'underline' },
     { tag: t.strikethrough, textDecoration: 'line-through' },
-    { tag: t.heading, fontWeight: 'bold', color: palette.lavender.hex },
+    {
+      tag: t.inserted,
+      backgroundColor: new TinyColor(palette.green.hex).setAlpha(0.15).toHex8String(),
+    },
+    {
+      tag: t.deleted,
+      backgroundColor: new TinyColor(palette.red.hex).setAlpha(0.15).toHex8String(),
+    },
+    {
+      tag: t.changed,
+      backgroundColor: new TinyColor(palette.blue.hex).setAlpha(0.15).toHex8String(),
+    },
     { tag: t.invalid, color: palette.red.hex },
+    { tag: t.meta, color: palette.yellow.hex },
+    { tag: t.documentMeta, color: palette.yellow.hex },
+    { tag: t.annotation, color: palette.yellow.hex },
+    { tag: t.processingInstruction, color: palette.yellow.hex },
+    { tag: t.definition(t.name), color: palette.maroon.hex },
+    { tag: t.definition(t.propertyName), color: palette.lavender.hex },
+    { tag: t.constant(t.name), color: palette.peach.hex },
+    { tag: t.function(t.variableName), color: palette.blue.hex },
+    { tag: t.standard(t.name), color: palette.mauve.hex },
+    { tag: t.local(t.name), color: palette.flamingo.hex },
+    { tag: t.special(t.string), color: palette.pink.hex },
+    { tag: t.special(t.variableName), color: palette.pink.hex },
   ])
 }
 
